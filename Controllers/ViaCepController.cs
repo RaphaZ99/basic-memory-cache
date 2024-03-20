5f61bfb87027d76e91fd3bb1dc114c7f8c196fa2
Raphael M. Santana <raphael.mota14@hotmail.com>
2024-03-20 20:25:36 -0300

    Basic Memory Cache

---
+++ Controllers/ViaCepController.cs
@@ -0,0 +1,50 @@
+﻿using BasicMemoryCache.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
+
+namespace BasicMemoryCache.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/v1")]
+    public class ViaCepController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+
+        public ViaCepController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("zipCode/{zipCode}")]
+        public async Task<IActionResult> GetByZipCode(string zipCode)
+        {
+
+            const string urlViaCep = "https://viacep.com.br/ws";
+
+            if (_memoryCache.TryGetValue(zipCode, out ViaCep cepData))
+            {
+                return Ok(cepData);
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"{urlViaCep}/{zipCode}/json");
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                cepData = JsonSerializer.Deserialize<ViaCep>(responseData);
+
+
+                var memoryCacheEntryExpiration = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                    SlidingExpiration = TimeSpan.FromSeconds(3600)
+                };
+
+                _memoryCache.Set(zipCode, cepData, memoryCacheEntryExpiration);
+                return Ok(cepData);
+            }
+        }
+    }
+}
---
+++ Model/ViaCep.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BasicMemoryCache.Model
+{
+    public class ViaCep
+    {
+        [JsonPropertyName("cep")]
+        public string ZipCode { get; set; }
+
+        [JsonPropertyName("logradouro")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("bairro")]
+        public string Neighborhood { get; set; }
+
+        [JsonPropertyName("localidade")]
+        public string Locality { get; set; }
+
+
+        [JsonPropertyName("uf")]
+        public string UF { get; set; }
+
+        [JsonPropertyName("gia")]
+        public string GIA { get; set; }
+
+        [JsonPropertyName("ddd")]
+        public string DDD { get; set; }
+
+        [JsonPropertyName("siafi")]
+        public string Siafi { get; set; }
+    }
+}
